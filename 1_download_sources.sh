0bf8f3a6c411c623d37142db31e349872587ed66
mammo0 <marc.ammon@hotmail.de>
2023-11-30 14:18:12 +0100

    modifications for Qnap GPL version 5.1.0
    with this version the tar files
    - have a new name
    - consit of multiple parts

--- 1_download_sources.sh
+++ 1_download_sources.sh
@@ -6,15 +6,47 @@
 QNAP_ARCHIVE="GPL_QTS-${QNAP_VER}_Kernel.tar.gz"
 
 
+function _url_exists() {
+    ret_code=$(curl -sLIk -o /dev/null -w "%{http_code}" "$1")
+
+    [ "$ret_code" -eq "200" ]
+}
+
+
 function build() {
     pushd "$SRC_DIR"
 
     if [[ ! -d "$QNAP_DIR" ]]; then
-        # get the QNAP source
-        curl -LJOk "https://sourceforge.net/projects/qosgpl/files/QNAP%20NAS%20GPL%20Source/QTS%20${QNAP_VER:0:5}/GPL_QTS-${QNAP_VER}_Kernel.tar.gz/download"
+        single_tar_url="https://sourceforge.net/projects/qosgpl/files/QNAP%20NAS%20GPL%20Source/QTS%20${QNAP_VER:0:5}/GPL_QTS-${QNAP_VER}_Kernel.tar.gz"
 
-        tar -zxf "$QNAP_ARCHIVE"
-        rm "$QNAP_ARCHIVE"
+        # check the single tar file
+        if _url_exists "$single_tar_url"; then
+            # get single tar file
+            curl -Lk "$single_tar_url" -o "$QNAP_ARCHIVE"
+
+            tar -zxf "$QNAP_ARCHIVE"
+            rm "$QNAP_ARCHIVE"
+        else
+            file_counter=0
+            while true
+            do
+                # set the name for the split tar file
+                split_tar_url="https://sourceforge.net/projects/qosgpl/files/QNAP%20NAS%20GPL%20Source/QTS%20${QNAP_VER:0:5}/QTS_Kernel_${QNAP_VER}.tar.gz.${file_counter}"
+
+                # stop here if the file does not exits
+                if ! _url_exists "$split_tar_url"; then
+                    break
+                fi
+
+                # get the split tar file
+                curl -Lk "$split_tar_url" -o "${QNAP_ARCHIVE}.${file_counter}"
+
+                file_counter=$((file_counter + 1))
+            done
+
+            cat "${QNAP_ARCHIVE}."* | tar -zxf -
+            rm "${QNAP_ARCHIVE}."*
+        fi
     fi
 
     # copy the kernel config to the kernel directory
